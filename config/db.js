8a4bfd2b312a16df071206225e89e8bb0e990e59
fabrianyo <fabrianyo@gmail.com>
2022-03-29 13:41:30 -0500

    cambios de seguridad

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-const DB_URI = 'mongodb+srv://fabriany123:Mercurio.2022@cluster0.gikzy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
+const DB_URI = process.env.MONGO_URL;
 module.exports = () => {
   const connect = () => {
     mongoose.connect(
-      DB_URI,
+      DB_URI.replace(/["']+/g, ''),
       {
         keepAlive: true,
         useNewUrlParser: true,
@@ -13,6 +12,8 @@ module.exports = () => {
       },
       (err) => {
         if (err) {
+
+          console.log('DB_URI: ' + DB_URI);
           console.log('Error de conexon a DB: ' + err);
         } else {
           console.log('Conexion Correcta!!!');
